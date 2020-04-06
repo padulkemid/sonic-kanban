b2150607e33f9fd2a3bbebfa971b542762e6e266
Fadhil Muhammad <bringitdown0@gmail.com>
2020-04-06 21:41:56 +0700

    db config fix

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
+  production: {
     username: process.env.DB_USER,
     password: process.env.DB_SECRET,
     database: process.env.DB_NAME,
@@ -10,11 +10,10 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    operatorsAliases: false,
+    username: 'ftghgpticlklby',
+    password: '71e945f9dd8dc3c6a7a1c54545dd6747e0ce7306c24a60b122a4f9d5e2a45546',
+    database: 'd2ouedpvfasb7p',
+    host: 'ec2-52-202-185-87.compute-1.amazonaws.com',
+    dialect: 'postgres',
   },
 };
